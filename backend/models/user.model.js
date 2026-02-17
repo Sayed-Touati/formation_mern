e2a85c1442b12482ddc83006efb184fe5c529836
Sayed Touati <sayedtouati@outlook.com>
2026-02-17 20:17:06 +0100

    feat: update user schema role enum
    changed roles and made admin as default

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -33,8 +33,8 @@ const userSchema = new mongoose.Schema(
         },
         role: {
             type: String,
-            enum: ["ADMIN", "USER"],
-            default: "USER"
+            enum: ["ADMIN", "PATIENT", "DOCTOR"],
+            default: "ADMIN"
         }
     }, { timestamps: true })
 
