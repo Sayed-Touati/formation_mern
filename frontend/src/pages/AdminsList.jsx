49457f6fd03e548d665d578412b0890474c1b36e
Sayed Touati <sayedtouati@outlook.com>
2026-02-19 14:41:34 +0100

    feat: implemented users data in table and added actions column CTA btn

--- frontend/src/pages/AdminsList.jsx
+++ frontend/src/pages/AdminsList.jsx
@@ -1,13 +1,75 @@
-import React from 'react'
+import { Button, Divider, Space, Table, Tag, message } from "antd"
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
+import { format } from "date-fns"
+
+const columns = [
+   {
+      title: "Created At",
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      render: text => format(text, 'yyyy-MM-dd HH:mm')
+   },
+   {
+      title: 'First Name',
+      dataIndex: 'firstName',
+      Key: 'firstName'
+   },
+   {
+      title: 'Last Name',
+      dataIndex: 'lastName',
+      Key: 'lastName'
+   },
+   {
+      title: 'Email',
+      dataIndex: 'email',
+      Key: 'email'
+   },
+   {
+      title: 'Account Type',
+      dataIndex: 'role',
+      Key: 'role',
+      render: text => <Tag color={text === 'ADMIN' ? 'geekblue': 'green'}>{text}</Tag>
+   },
+   {
+      title: 'Actions',
+      render: (_) => (
+         <Space>
+            <Button type="link" size="small">Details</Button>
+            <Button type="primary" size="small">Edit</Button>
+         </Space>
+      )
+   },
+]
 
 const AdminsList = () => {
+
+   const [users, setUsers] = useState([])
+
+   useEffect(() => {
+      async function fetchData() {
+         try {
+            const response = await axios.get('http://localhost:3000/user/')
+
+            setUsers(response.data.users)
+         } catch (error) {
+            console.log(error.response)
+            message.error(error.response.data.message)
+         }
+      }
+      fetchData()
+   }, [])
+
   return (
     <div>
       <h4>
         Admins List
       </h4>
+      <Divider />
+      <Table columns={columns} dataSource={users} scroll={{ x: 'max-content' }} />
     </div>
   )
 }
 
+// email, firstName, lastName, dob, role, createdAt
 export default AdminsList
